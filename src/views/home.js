99492d42b0750c371bb2a798f57e67f584e95f71
Hugo <chautardhugo@gmail.com>
2024-03-10 21:13:52 +0100

    Erased test

--- src/views/home.js
+++ src/views/home.js
@@ -7,12 +7,11 @@ function Home() {
   return (
     <div>
       <HeaderHome />
-      <div>GORS TESTZEFAZRZAR</div>
       <div className="container slide-top">
         <h1>Votre temps c'est de l'argent.</h1>
         <h3>Planifiez vos dépenses, définissez des objectifs d'épargne et suivez votre progression avec des graphiques clairs et simples. InTime vous permet de visualiser instantanément vos dépenses en temps réel.</h3>  
         <img id="euro_floating" src={euro_3d_floating} alt="euro_floating" className="euro_floating"/>
-      </div>
+      </div> 
       <FooterHome />
     </div>
   );
